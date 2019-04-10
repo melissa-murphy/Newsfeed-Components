29f28e323db55a348ceda103543ee65a72cb688d
melissa-murphy <melissamurphy.ca@gmail.com>
2019-04-10 14:36:13 -0700

    buttons exist!

--- Article/Article.js
+++ Article/Article.js
@@ -1,13 +1,17 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(articleTemplate) {
     // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
+    this.domElement = articleTemplate;
     // create a reference to the ".expandButton" class. 
     this.expandButton = '.expandButton';
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    
+    const button = document.createElement('button');
+    const buttonText = document.createTextNode('expand');
+
+    button.appendChild(buttonText);
+    document.querySelector('.expandButton').appendChild(button);
     // Set a click handler on the expandButton reference, calling the expandArticle method.
     
   }
